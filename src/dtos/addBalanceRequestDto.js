e9d4f64efd5d78b9e147423f2f5f5b8abb3bf92e
Alvaro Martin <alvaro.martin@tadaima.studio>
2021-11-17 19:31:02 +0100

    feat: Add addBlanceRequestDto

---
+++ src/dtos/addBalanceRequestDto.js
@@ -0,0 +1,13 @@
+class AddBalanceRequestDto {
+  constructor(amount) {
+    if (amount <= 0) {
+      throw new Error('Amount to add must be greater than 0');
+    }
+
+    return {
+      amount,
+    };
+  }
+}
+
+module.exports = AddBalanceRequestDto;
