4a3fa2628a037af9596e71bcf7adc5c522583408
Alvaro Martin <alvaro.martin@tadaima.studio>
2021-11-18 07:57:08 +0100

    test. Add addBalanceRequestDto unit test

---
+++ tests/unit/dtos/addBalanceRequestDto.test.js
@@ -0,0 +1,24 @@
+const AddBalanceRequestDto = require('../../../src/dtos/addBalanceRequestDto');
+const { NoErrorThrownError, getError } = require('../errors/noErrorThrownError');
+
+describe('userRequestDto tests', () => {
+  test('Given less than 0 amount When call constructor Then should throw an error', async () => {
+    const error = await getError(async () => new AddBalanceRequestDto(-0.1));
+
+    expect(error).not.toBeInstanceOf(NoErrorThrownError);
+    expect(error).toHaveProperty('message', 'Amount to add must be greater than 0');
+  });
+
+  test('Given equals to 0 amount When call constructor Then should throw an error', async () => {
+    const error = await getError(async () => new AddBalanceRequestDto(0));
+
+    expect(error).not.toBeInstanceOf(NoErrorThrownError);
+    expect(error).toHaveProperty('message', 'Amount to add must be greater than 0');
+  });
+
+  test('Given greater than 0 amount When call constructor Then should return DTO object', async () => {
+    const addBalanceRequestDto = new AddBalanceRequestDto(0.01);
+
+    expect(addBalanceRequestDto.amount).toBe(0.01);
+  });
+});
